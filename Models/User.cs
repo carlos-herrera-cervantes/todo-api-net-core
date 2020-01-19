c9c77b880cd1323e70b3565bff2aac159c72c0b9
Carlos Herrera <carlos.cervantes.cp@gmail.com>
2020-01-18 20:54:35 -0600

    First commit

---
+++ Models/User.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TodoApiNet.Models
+{
+    public class User
+    {
+        [Required]
+        public long Id { get; set; }
+
+        [Required]
+        public string FirstName { get; set; }
+
+        [Required]
+        public string LastName { get; set; }
+
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(50, MinimumLength = 6)]
+        public string Password { get; set; }
+    }
+}
