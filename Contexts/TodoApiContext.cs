343165e07662dd053bed4a56dedca7acbf371e32
Carlos Herrera <carlos.cervantes.cp@gmail.com>
2020-01-18 21:24:54 -0600

    Add database configuration

---
+++ Contexts/TodoApiContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TodoApiNet.Models;
+
+namespace TodoApiNet.Contexts
+{
+    public class TodoApiContext : DbContext
+    {
+        public TodoApiContext(DbContextOptions<TodoApiContext> options) : base(options) { }
+
+        public DbSet<User> Users { get; set; }
+
+        public DbSet<Todo> Todos { get; set; }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -1,24 +1,25 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using TodoApiNet.Contexts;
 
 namespace TodoApiNet
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public IConfiguration Configuration { get; set; }
+
+        public Startup(IConfiguration configuration) => Configuration = configuration;
+
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<TodoApiContext>(options => options.UseSqlite(Configuration["ConnectionStrings:default"]));
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
